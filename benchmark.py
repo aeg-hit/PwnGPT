ae0eb44de82dada647dd32ba0d3b4719d2204184
Peng <605144885@qq.com>
2024-11-12 20:52:07 +0800

    evaluate 2

--- benchmark.py
+++ benchmark.py
@@ -7,6 +7,7 @@ from pprint import pprint
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
+
 def evaluate_0():
     # evaluate 0: pure llm without reflect (flag = "do not reflect" max_iterations=1)
     # pwn_path=file.PwnInfo("./pwn/stack/", "rop")
@@ -23,19 +24,22 @@ def evaluate_0():
         with open(pwn_path.list[i]+f'/result_1_{llmgraph.expt_llm}.txt', 'w') as f:
             pprint(resultcode, stream=f)
 
-llm = ChatOpenAI(temperature=0, model=llmgraph.expt_llm, base_url=llmgraph.base)
+
+llm = ChatOpenAI(temperature=0, model=llmgraph.expt_llm,
+                 base_url=llmgraph.base)
 code_gen_prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """You are a expert on Capture the Flag (CTF) competition, and are good at Binary Exploitation (pwn) challenges. \n 
+    [
+        (
+            "system",
+            """You are a expert on Capture the Flag (CTF) competition, and are good at Binary Exploitation (pwn) challenges. \n 
         There is a pwn challenge in the CTF competition, and here is information about the challenge to you for analysis:  \n ------- \n  {context} \n ------- \n
           Answer the user question based on the above provided information. Here is the user question:""",
-            ),
-            ("placeholder", "{messages}"),
-        ]
-    )
+        ),
+        ("placeholder", "{messages}"),
+    ]
+)
 gen_chain = code_gen_prompt | llm
+
 
 def evaluate_1():
     # evaluate 1: key information analysis
@@ -47,11 +51,11 @@ def evaluate_1():
 
     check_result = subprocess.run(['checksec', '--format=json', '--file='+blist[i]],
                                   check=True, capture_output=True, text=True)
-    
+
     pprint(blist[i])
     contest = f"\nHere is the file infomation created by 'file' command:\n{file_result.stdout}\n\nHere is the file security infomation created by 'checksec' command:\n{check_result.stdout}"
-    question="Please analyse these information."
-    resultcode=gen_chain.invoke(
+    question = "Please analyse these information."
+    resultcode = gen_chain.invoke(
         {"context": contest, "messages": [("user", question)]}
     )
     print(resultcode)
@@ -60,6 +64,28 @@ def evaluate_1():
         print(resultcode.content, stream=f)
 
 
+def evaluate_2():
+    # vulnerability exploitation point location
+    pwn_path = file.PwnInfo("./pwn/stack/", "rop")
+    # pwn_path=file.PwnInfo("./pwn/string/", "fmt")
+    # pwn_path = file.PwnInfo("./pwn/integer/", "int")
+    clist = pwn_path.get_clist()
+    print("Start: ")
+    for i in range(len(clist)):
+        print(clist[i])
+        decfile = llmgraph.get_decompilefile(clist[i])[0]
+        c_infohead = "\nHere is the decompiled C file:\n"
+        question = "What vulnerabilities exist in the code? Please tell me the location and type of vulnerabilities."
+        resultcode = gen_chain.invoke(
+            {"context": c_infohead+decfile.page_content,
+                "messages": [("user", question)]}
+        )
+        print(resultcode)
+        # save
+        with open(pwn_path.list[i]+f'/evaluate_2_{llmgraph.expt_llm}.txt', 'w') as f:
+            print(resultcode.content, file=f)
+        break
+
 
 if __name__ == "__main__":
-    evaluate_1()
+    evaluate_2()
